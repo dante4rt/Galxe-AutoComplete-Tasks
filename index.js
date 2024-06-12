89153a38ca3ffb7efc3fccee2b0802f7b289a140
Dante4rt <rxmxdhxni@gmail.com>
2024-06-12 23:34:30 +0700

    feat: update based on new Galxe

--- index.js
+++ index.js
@@ -1,119 +1,125 @@
-const _0x598795 = _0x22dd;
-(function (_0x3bd982, _0x495c14) {
-  const _0x4dd2b = _0x22dd,
-    _0x529f37 = _0x3bd982();
+const _0x36922a = _0x497f;
+function _0x5274() {
+  const _0x516b42 = [
+    'button.inline-flex.bg-primary:not([disabled])',
+    'button.absolute.rounded-sm.opacity-70.right-5.top-6.sm\x5c:right-9.sm\x5c:top-9',
+    '.text-size-14.font-bold',
+    '35gQwDne',
+    '277620yrXTYP',
+    '693212uRArZT',
+    '10XiPEYc',
+    'length',
+    'querySelector',
+    '263061TXsVpZ',
+    'log',
+    '9xXLime',
+    '562428MoGfya',
+    'querySelectorAll',
+    'click',
+    '443272wviMjY',
+    '2055752JchJhQ',
+    'dispatchEvent',
+    '6919KBZYzm',
+    'svg[data-state=\x22closed\x22]\x20.ml-4.flex.gap-4.items-center',
+    '12030eRCjzt',
+    'forEach',
+  ];
+  _0x5274 = function () {
+    return _0x516b42;
+  };
+  return _0x5274();
+}
+(function (_0x2cbd1c, _0x13e489) {
+  const _0x1e795c = _0x497f,
+    _0x46de30 = _0x2cbd1c();
   while (!![]) {
     try {
-      const _0x30dc0e =
-        -parseInt(_0x4dd2b(0x136)) / 0x1 +
-        (-parseInt(_0x4dd2b(0x139)) / 0x2) * (parseInt(_0x4dd2b(0x132)) / 0x3) +
-        parseInt(_0x4dd2b(0x128)) / 0x4 +
-        parseInt(_0x4dd2b(0x13c)) / 0x5 +
-        (-parseInt(_0x4dd2b(0x12c)) / 0x6) * (parseInt(_0x4dd2b(0x133)) / 0x7) +
-        (parseInt(_0x4dd2b(0x134)) / 0x8) * (parseInt(_0x4dd2b(0x13a)) / 0x9) +
-        (-parseInt(_0x4dd2b(0x126)) / 0xa) * (-parseInt(_0x4dd2b(0x131)) / 0xb);
-      if (_0x30dc0e === _0x495c14) break;
-      else _0x529f37['push'](_0x529f37['shift']());
-    } catch (_0x596818) {
-      _0x529f37['push'](_0x529f37['shift']());
+      const _0x4161ea =
+        -parseInt(_0x1e795c(0x190)) / 0x1 +
+        parseInt(_0x1e795c(0x18c)) / 0x2 +
+        parseInt(_0x1e795c(0x193)) / 0x3 +
+        (-parseInt(_0x1e795c(0x196)) / 0x4) *
+          (-parseInt(_0x1e795c(0x18d)) / 0x5) +
+        (-parseInt(_0x1e795c(0x18b)) / 0x6) *
+          (-parseInt(_0x1e795c(0x1a0)) / 0x7) +
+        (parseInt(_0x1e795c(0x197)) / 0x8) *
+          (parseInt(_0x1e795c(0x192)) / 0x9) +
+        (-parseInt(_0x1e795c(0x19b)) / 0xa) *
+          (parseInt(_0x1e795c(0x199)) / 0xb);
+      if (_0x4161ea === _0x13e489) break;
+      else _0x46de30['push'](_0x46de30['shift']());
+    } catch (_0x49686e) {
+      _0x46de30['push'](_0x46de30['shift']());
     }
   }
-})(_0xbcef, 0x43cc1);
-const elementsToClick = document['querySelectorAll'](_0x598795(0x138)),
-  refreshButtonClass = '.clickable.refresh.icon.responsive.ml-4',
-  waitForClass = _0x598795(0x129),
-  successIconClass = _0x598795(0x12b),
-  claimButtonSelector = _0x598795(0x12e),
-  closeButtonSelector = _0x598795(0x13b),
-  message = _0x598795(0x125);
-function clickElement(_0x4e773a) {
-  const _0x444a74 = _0x598795,
-    _0x1010ec = new MouseEvent(_0x444a74(0x12a), {
+})(_0x5274, 0x36c21);
+const elementsToClick = document[_0x36922a(0x194)](
+    'div[data-state=\x22closed\x22]\x20.cursor-pointer'
+  ),
+  refreshButtonClass =
+    'button[data-state=\x22closed\x22]\x20.ml-4.flex.gap-4.items-center',
+  waitForClass = _0x36922a(0x19f),
+  successIconClass = _0x36922a(0x19a),
+  claimButtonSelector = _0x36922a(0x19d),
+  closeButtonSelector = _0x36922a(0x19e);
+function clickElement(_0x2208d8) {
+  const _0x539b18 = _0x36922a,
+    _0x3c7c78 = new MouseEvent(_0x539b18(0x195), {
       view: window,
       bubbles: !![],
       cancelable: !![],
     });
-  _0x4e773a['dispatchEvent'](_0x1010ec);
+  _0x2208d8[_0x539b18(0x198)](_0x3c7c78);
 }
 function closePopup() {
-  const _0x29e401 = _0x598795,
-    _0x65a39e = document['querySelector'](closeButtonSelector);
-  _0x65a39e &&
-    (clickElement(_0x65a39e), console[_0x29e401(0x12d)](_0x29e401(0x127)));
+  const _0x4249bb = _0x36922a,
+    _0x2da1ac = document[_0x4249bb(0x18f)](closeButtonSelector);
+  _0x2da1ac &&
+    (clickElement(_0x2da1ac), console[_0x4249bb(0x191)]('Popup\x20closed.'));
 }
 function checkClaimButton() {
-  const _0x664909 = _0x598795,
-    _0x692d76 = document[_0x664909(0x135)](claimButtonSelector),
-    _0x358b22 = atob(message);
-  if (_0x692d76)
+  const _0x2e8229 = _0x36922a,
+    _0x49c844 = document['querySelector'](claimButtonSelector);
+  if (_0x49c844)
     return (
-      clickElement(_0x692d76),
-      console[_0x664909(0x12d)](_0x358b22),
+      clickElement(_0x49c844),
+      console[_0x2e8229(0x191)](
+        'Claim\x20button\x20clicked.\x20Subscribe:\x20https://t.me/HappyCuanAirdrop'
+      ),
       setTimeout(closePopup, 0x7d0),
       !![]
     );
   return ![];
 }
 function checkConditionsAndRetry() {
-  const _0x1b9795 = _0x598795,
-    _0x134809 = document[_0x1b9795(0x12f)](waitForClass),
-    _0x210400 = document[_0x1b9795(0x12f)](successIconClass);
-  _0x134809[_0x1b9795(0x137)] > 0x0 ||
-  _0x210400['length'] !== elementsToClick[_0x1b9795(0x137)]
+  const _0x2eae8d = _0x36922a,
+    _0x6dd6b3 = document[_0x2eae8d(0x194)](waitForClass),
+    _0x605121 = document[_0x2eae8d(0x194)](successIconClass);
+  _0x6dd6b3[_0x2eae8d(0x18e)] > 0x0 ||
+  _0x605121[_0x2eae8d(0x18e)] !== elementsToClick['length']
     ? setTimeout(processElements, 0xea60)
     : checkClaimButton();
 }
-function _0x22dd(_0x504ca5, _0x349c9c) {
-  const _0xbcef4e = _0xbcef();
+function _0x497f(_0xa67890, _0x1f3538) {
+  const _0x527456 = _0x5274();
   return (
-    (_0x22dd = function (_0x22dd85, _0xde8c14) {
-      _0x22dd85 = _0x22dd85 - 0x125;
-      let _0xc33ad5 = _0xbcef4e[_0x22dd85];
-      return _0xc33ad5;
+    (_0x497f = function (_0x497fd2, _0x7ff795) {
+      _0x497fd2 = _0x497fd2 - 0x18b;
+      let _0x5f2151 = _0x527456[_0x497fd2];
+      return _0x5f2151;
     }),
-    _0x22dd(_0x504ca5, _0x349c9c)
+    _0x497f(_0xa67890, _0x1f3538)
   );
 }
 function processElements() {
+  const _0x20a19a = _0x36922a;
   !checkClaimButton() &&
-    (elementsToClick['forEach'](clickElement),
+    (elementsToClick[_0x20a19a(0x19c)](clickElement),
     setTimeout(() => {
-      const _0x505ab5 = _0x22dd,
-        _0x1ace0b = document[_0x505ab5(0x12f)](refreshButtonClass);
-      _0x1ace0b[_0x505ab5(0x130)](clickElement),
+      const _0x3c2249 = _0x20a19a,
+        _0x5a20c6 = document[_0x3c2249(0x194)](refreshButtonClass);
+      _0x5a20c6[_0x3c2249(0x19c)](clickElement),
         setTimeout(checkConditionsAndRetry, 0x7d0);
     }, 0x7d0));
 }
 processElements();
-function _0xbcef() {
-  const _0xc55527 = [
-    'button.g-btn.width-max-100:not(.v-btn--disabled)',
-    'querySelectorAll',
-    'forEach',
-    '11beFvEx',
-    '201093nnPmEE',
-    '7lFVQmC',
-    '4248eFJwGN',
-    'querySelector',
-    '536046RjGQGD',
-    'length',
-    'button.v-expansion-panel-header\x20.d-flex.height-100.width-100.click-area',
-    '10pvrxjY',
-    '1350vCnUbV',
-    'img.trp-close',
-    '2713210MmwoFh',
-    'Q2xhaW0gYnV0dG9uIGNsaWNrZWQuIFN1YnNjcmliZTogaHR0cHM6Ly90Lm1lL0hhcHB5Q3VhbkFpcmRyb3A=',
-    '7369730DZwfQr',
-    'Popup\x20closed.',
-    '1214380nfuqRc',
-    '.text-lighten-2.text-14-regular.ml-4',
-    'click',
-    'img.icon.ml-4',
-    '3083772akIqzz',
-    'log',
-  ];
-  _0xbcef = function () {
-    return _0xc55527;
-  };
-  return _0xbcef();
-}
